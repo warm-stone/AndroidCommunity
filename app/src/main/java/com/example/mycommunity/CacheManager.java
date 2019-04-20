9152ceed6595a9fcf39260161bcf6524416e7072
Hanscang <hzqsdnu@qq.com>
2019-04-20 16:17:01 +0800

    cacheTest

--- app/src/main/java/com/example/mycommunity/CacheManager.java
+++ app/src/main/java/com/example/mycommunity/CacheManager.java
@@ -1,5 +1,78 @@
 package com.example.mycommunity;
 
-public class CacheManager {
+import org.litepal.LitePal;
+import org.litepal.crud.LitePalSupport;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CacheManager<T extends LitePalSupport> {
+
+    Class<T> clazz;
+
+    public CacheManager() {
+        clazz = new RawDao<T>().clazz;
+    }
+
+    /*
+     * 存储缓存并检查是否已存在
+     * Save cache and check the exist
+     *
+     * @return 缓存中是否已存在
+     * */
+    public boolean saveData(List<T> cache) {
+        for (T t : cache) {
+            t.save();
+        }
+        return false;
+    }
+
+    /*
+     * 获取缓存数据并返回是否已到最后一条
+     * Get cache and return true if the data exist.
+     * @deviation 偏移量
+     * @page 页码
+     * @size 页面规格
+     *
+     * @return 获取数据是否存在
+     *         whether the data exists.
+     *
+     * */
+    public List<T> getData(int deviation, int page, int size) {
+        List<T> data = new ArrayList<>();
+
+        return data;
+    }
+
+    public T getLast() {
+        return LitePal.findFirst(clazz);
+    }
+
+    public T getFirst() {
+        return LitePal.findLast(clazz);
+    }
+}
+
+class RawDao<T> {
+    Class<T> clazz;
+
+    @SuppressWarnings("unchecked")
+    RawDao() {
+        @SuppressWarnings("rawtypes")
+        Class clazz = getClass();
+
+        while (clazz != Object.class) {
+            Type t = clazz.getGenericSuperclass();
+            if (t instanceof ParameterizedType) {
+                Type[] args = ((ParameterizedType) t).getActualTypeArguments();
+                if (args[0] instanceof Class) {
+                    this.clazz = (Class<T>) args[0];
+                    break;
+                }
+            }
+            clazz = clazz.getSuperclass();
+        }
+    }
 }
